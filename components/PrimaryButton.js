2a34239b4e236b382914e6644716c2efad001efb
gosain <alksmaniac@gmail.com>
2023-07-03 14:05:37 -0600

    se agregan estilos a los componentes

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,5 +1,18 @@
- PrimaryButton = () => {
-    return
+ import { View, Text, Pressable } from "react-native";
+
+ PrimaryButton = ({children}) => {
+  
+  pressHandler = () => {
+    console.log('Pressed!');
+  }
+    return(
+      <Pressable onPress={pressHandler}>
+        <View>
+          <Text>{children}</Text>
+        </View>
+      </Pressable>
+
+    );
  }
 
  export default PrimaryButton;

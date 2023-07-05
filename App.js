78ad46a111f2cc2b041c7d7737838619c07923e8
gosain <alksmaniac@gmail.com>
2023-07-05 14:18:43 -0600

    add gradient package and change background color

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import { StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 import StartGameScreen from './screens/StartGameScreen';
 
+
 export default function App() {
-  return <StartGameScreen />;
+  return(
+    <LinearGradient colors={['#72063c','#ddb52f']} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
+
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen:{
+    flex: 1,
+    backgroundColor:'#ddb52f'
+  }
+});
